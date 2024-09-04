adc7faa6c1a4df54b0a5126e05e5b0b51d76632b
rox-dotcom <roxarandagr@gmail.com>
2024-09-03 23:55:53 -0600

    Se añadio una funcion para imprimir

--- proyect.py
+++ proyect.py
@@ -1,7 +1,6 @@
 import parte1 as p1
 
-
-def main():
+def printParte1():
     print("Transmission 1: ")
     print("mcode1: ")
     p1.analyze_files('./texts/transmission1.txt','./texts/mcode1.txt')
@@ -18,6 +17,9 @@ def main():
     p1.analyze_files('./texts/transmission2.txt','./texts/mcode2.txt')
     print("mcode3: ")
     p1.analyze_files('./texts/transmission2.txt','./texts/mcode3.txt')
+
+def main():
+    printParte1()
 
 if __name__ == "__main__":
     main()
